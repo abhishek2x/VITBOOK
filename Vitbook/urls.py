bb988e89b54dac8da1dd498530a99340c791cb36
Abhishek Srivastava <53976003+abhishek2x@users.noreply.github.com>
2020-10-21 00:47:21 +0530

    removing welcome path

--- Vitbook/urls.py
+++ Vitbook/urls.py
@@ -9,8 +9,8 @@ from social import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.default.urls')),                 
-    path('social/', include('social.urls')), 
-    path('', views.welcome.as_view()),            
+    path('', include('social.urls')), 
+    # path('', views.welcome.as_view()),            
     # path('accounts/register', views.register, name='register'), 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
